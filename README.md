e5cf2f49ec433f21636fbcc5e748f969b5ac61f9
Josh Ladd <joshua.ladd@colorado.edu>
2020-03-03 16:04:02 -0700

    trying this again

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bots as a Service
+### Joshua Ladd, Kamiar Coffey, Evan Lee
+
+
+__Project Description__
+We seek to develop a web application to facilitate the easy modular construction of reddit bots through a GUI as well as the potential to deploy and host these bots. 
+
+__Current Stack__
+We currently plan to deploy this app using Kotlin, with the spin-up of the bots being serverless through AWS Lambda and a service to deploy these bots through Digital Ocean. 
+
+
+
+
+
---
+++ src/Application.kt
@@ -0,0 +1,110 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import freemarker.cache.*
+import io.ktor.freemarker.*
+import io.ktor.features.*
+import io.ktor.http.content.PartData
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import org.slf4j.event.*
+
+fun main() {
+    //val port = System.getenv("PORT")?.toInt() ?: 8080
+
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondHtml {
+                    head {
+                        title { +"The Bad Bot Name Generator" }
+                    }
+                    body {
+                        p {
+                            +"The Bad Bot Name Generator"
+                        }
+                        form("/generate", encType = FormEncType.textPlain, method = FormMethod.post) {
+                            acceptCharset = "ascii"
+                            p {
+                                label { +"Number of Names:" }
+                                textInput { name = "textField" }
+                            }
+                            p {
+                                submitInput { value = "Generate Names" }
+                            }
+                        }
+                    }
+                }
+            }
+
+            post("/generate") {
+
+                var multipart = call.receiveText()
+                var temp = multipart.split('=')[1].trim().toIntOrNull()
+                if (temp !is Int) {
+                    call.respondHtml {
+                        head {
+                            title { +"The Bad Bot Name Generator" }
+                        }
+                        body {
+                            h3 {
+                                +"Generated $temp Bot Names\n"
+                            }
+
+                            p {
+                                +"Oh wait, $temp isn't an integer!"
+                            }
+
+
+                            form("/", encType = FormEncType.textPlain, method = FormMethod.get) {
+                                acceptCharset = "ascii"
+                                p {
+                                    submitInput { value = "Try Again" }
+                                }
+                            }
+                        }
+                    }
+                }
+                else {
+                    call.respondHtml {
+                        head {
+                            title { +"The Bad Bot Name Generator" }
+                        }
+                        body {
+                            h3 {
+                                +"Generated $temp Bot Names\n"
+                            }
+
+                            val firstnames = arrayOf("lemon", "heavy", "batty", "cheeky", "cruel", "elegant", "greasy", "lucky")
+                            val lastnames = arrayOf("money", "picture", "box", "bear", "window", "tree", "pigeon", "apple")
+                            for (i in 1..temp) {
+                                val randomInteger = (0..7).shuffled()
+                                val bot_name = i.toString() + ". " + firstnames[randomInteger[0]] + " " + lastnames[randomInteger[1]]
+                                p {
+                                    +bot_name
+                                }
+                            }
+
+                            form("/", encType = FormEncType.textPlain, method = FormMethod.get) {
+                                acceptCharset = "ascii"
+                                p {
+                                    submitInput { value = "Generate More Names" }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }.start(wait = true)
+}
