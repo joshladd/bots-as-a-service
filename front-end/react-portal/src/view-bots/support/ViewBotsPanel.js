dab454e9707a9b472ffe0eb2fbc68cbfaca663ac
Archetypically <Archetypically@users.noreply.github.com>
2020-03-19 23:59:11 -0600

    refactor(front-end): Add dynamic expansion panel
    - Now passes data to ViewBotsPanel.
    - Now creates one ViewBotsPanel per entry in stubbed list.
    - Play button now colored success.
    - Add "quick-actions" to expansion panel summary.
    
    
    Former-commit-id: 10ff17f5b6542ddfd029edefa1a310f316f91bae

--- front-end/react-portal/src/view-bots/support/ViewBotsPanel.js
+++ front-end/react-portal/src/view-bots/support/ViewBotsPanel.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Divider from '@material-ui/core/Divider';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -19,35 +22,77 @@ const useStyles = makeStyles(theme => ({
   },
   heading: {
     fontSize: theme.typography.pxToRem(16),
+		color: theme.palette.text.primary,
     flexGrow: 1,
+		verticalAlign: 'baseline',
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
-    justifyContent: "flex-end"
-  },
-  deleteButton: {
-  	color: theme.palette.error.dark,
+    justifyContent: "flex-end",
+		marginRight: "1rem",
   },
   startButton: {
-  	color: theme.palette.primary.light,
+  	color: theme.palette.success.light,
   },
   pauseButton: {
   	color: theme.palette.primary.light,
+  },
+	deleteButton: {
+  	color: theme.palette.error.dark,
   },
 }));
 
 export default function ViewBotsPanel(props){
 	const classes = useStyles();
+	const botName = props.payload.name;
+	const isOnline = props.payload.status.online;
+
+	const enabledStatusLabel = isOnline ? 'active' : 'disabled';
+
+	const isPlayButtonDisabled = !isOnline;
+	const isPauseButtonDisabled = !isPlayButtonDisabled;
+
 	return (
-		<ExpansionPanel defaultExpanded>
+		<ExpansionPanel>
       <ExpansionPanelSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1a-content"
-        id={props.name + "-header"}
+        id={botName + "-header"}
       >
-        <Typography className={classes.heading}>{props.name}</Typography>
-        <Typography className={classes.secondaryHeading}>disabled</Typography>
+        <Typography className={classes.heading}>{botName}</Typography>
+        <Typography className={classes.secondaryHeading}>{enabledStatusLabel}</Typography>
+				<Divider orientation="vertical" flexItem />
+				<IconButton
+					aria-label="start-bot"
+					onClick={event => event.stopPropagation()}
+          onFocus={event => event.stopPropagation()}
+					className={classes.startButton}
+					disabled={isPlayButtonDisabled}
+					size="small"
+				>
+					<PlayArrowIcon fontSize="small"/>
+				</IconButton>
+				<IconButton
+					aria-label="pause-bot"
+					onClick={event => event.stopPropagation()}
+          onFocus={event => event.stopPropagation()}
+					className={classes.pauseButton}
+					disabled={isPauseButtonDisabled}
+					size="small"
+				>
+					<PauseIcon fontSize="small"/>
+				</IconButton>
+				<IconButton
+					aria-label="delete-bot"
+					onClick={event => event.stopPropagation()}
+          onFocus={event => event.stopPropagation()}
+					className={classes.deleteButton}
+					size="small"
+					disabled={isPlayButtonDisabled}
+				>
+					<DeleteIcon fontSize="small" />
+				</IconButton>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
         <Typography>
@@ -57,23 +102,13 @@ export default function ViewBotsPanel(props){
       </ExpansionPanelDetails>
       <Divider />
       <ExpansionPanelActions>
-      	<Button
-	        className={classes.startButton}
-	        startIcon={<PlayArrowIcon />}
-	      >
+      	<Button className={classes.startButton} startIcon={<PlayArrowIcon />} disabled={isPlayButtonDisabled}>
 	        Start
 	      </Button>
-	      <Button
-	        className={classes.pauseButton}
-	        startIcon={<PauseIcon />}
-	        disabled
-	      >
+	      <Button className={classes.pauseButton} startIcon={<PauseIcon />} disabled={isPauseButtonDisabled}>
 	        Disable
 	      </Button>
-        <Button
-	        className={classes.deleteButton}
-	        startIcon={<DeleteIcon />}
-	      >
+        <Button className={classes.deleteButton} startIcon={<DeleteIcon />}>
 	        Delete
 	      </Button>
       </ExpansionPanelActions>
