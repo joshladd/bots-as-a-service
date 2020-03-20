10ff17f5b6542ddfd029edefa1a310f316f91bae
Archetypically <Archetypically@users.noreply.github.com>
2020-03-19 23:59:11 -0600

    refactor(front-end): Add dynamic expansion panel
    - Now passes data to ViewBotsPanel.
    - Now creates one ViewBotsPanel per entry in stubbed list.
    - Play button now colored success.
    - Add "quick-actions" to expansion panel summary.

--- front-end/react-portal/src/view-bots/ViewBotsPortal.js
+++ front-end/react-portal/src/view-bots/ViewBotsPortal.js
@@ -14,14 +14,84 @@ export default function ViewBotsPortal(){
 	document.title = 'bots-as-a-service: bots';
 	// eslint-disable-next-line
 	const classes = useStyles();
+
+	const botsList = fakeResponse.map((payload) =>
+		<ViewBotsPanel key={payload.name} payload={payload} />
+	)
+
 	return (
 		<React.Fragment>
 			<CssBaseline />
 			<Container>
-				<ViewBotsPanel name="bot-1" />
-				<ViewBotsPanel name="bot-2" />
-				<ViewBotsPanel name="bot-3" />
+				{botsList}
 			</Container>
 		</React.Fragment>
 	);
 }
+
+// STUB
+const fakeResponse = [
+  {
+    "name": "stopped-bot01",
+    "auth" : {
+        "client_id": "",
+        "client_secret" : "",
+        "user_agent" : "",
+        "username": ""
+    },
+    "status" : {
+        "online": false,
+        "valid": true
+    },
+    "config" : {
+        "valid": true,
+        "subreddits" : ["test"],
+        "comments_enabled": true,
+        "livestream_enabled": false,
+        "comment_calling_syntax" : "!",
+        "services": []
+    }
+  },
+	{
+    "name": "healthy-bot02",
+    "auth" : {
+        "client_id": "",
+        "client_secret" : "",
+        "user_agent" : "",
+        "username": ""
+    },
+    "status" : {
+        "online": true,
+        "valid": true
+    },
+    "config" : {
+        "valid": true,
+        "subreddits" : ["test"],
+        "comments_enabled": true,
+        "livestream_enabled": false,
+        "comment_calling_syntax" : "!",
+        "services": []
+    }
+  },
+	{
+    "name": "invalid-bot03",
+    "auth" : {
+        "client_id": "",
+        "client_secret" : "",
+        "user_agent" : "",
+        "username": ""
+    },
+    "status" : {
+        "online": false,
+        "valid": false
+    },
+    "config" : {
+        "valid": true,
+        "subreddits" : ["test"],
+        "comments_enabled": true,
+        "livestream_enabled": false,
+        "comment_calling_syntax" : "!",
+        "services": []
+    }
+  }
+]
