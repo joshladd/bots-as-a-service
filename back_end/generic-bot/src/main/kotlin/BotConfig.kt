1566bf8453e1bd4dac2d2331906a620b6e136e44
Josh Ladd <joshua.ladd@colorado.edu>
2020-05-04 13:22:57 -0600

    removed kotlin reddit bot

--- back_end/generic-bot/src/main/kotlin/BotConfig.kt
+++
@@ -1,8 +0,0 @@
-data class BotConfig (
-    var subreddits : List<String>,
-    var comments_enabled : Boolean,
-    var livestream_enabled : Boolean,
-    var keyphrase : String,
-    var services: List<BotService>
-
-)
